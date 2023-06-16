d8b282c935d546161999f042a1fe01193763e2da
Vagner-Jr <vagnerflorindojunior@gmail.com>
2023-06-16 11:18:57 -0300

    separa as páginas e os arquivos css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const redirect = () => {
   let password = document.querySelector("#password").value;
 
   if (email == "ailuj@gmail.com" && password == 16062023) {
-    window.location.assign("index.html");
+    window.location.assign("/pages/home.html");
     alert("tudo ok!");
   } else {
     alert("email ou senha incorreta!");
